8d565c59ccc5b03e52929c7569f7b22ad4d012c9
Stas <ragioniere1980@gmail.com>
2022-04-19 21:39:40 +0300

    adout some events

--- script.js
+++ script.js
@@ -1,1 +1,60 @@
 "use strict";
+
+const btns = document.querySelectorAll('button'),
+      wrapper = document.querySelector('.btn-block');
+
+
+// console.log(btns[0].classList.length); // получаем кол-во классов
+
+// console.log(btns[0].classList.item(1)); // классы получаем
+
+// console.log(btns[1].classList.add('red', 'tttttttt')); // классы добавляем? можно несколько
+
+// // console.log(btns[0].classList.remove('blue_some')); // классы удаляем
+
+// console.log(btns[0].classList.toggle('blue_some')); // классы переключаем
+
+// if (btns[1].classList.contains('red')){
+//     console.log('Ура');
+// }
+
+btns[0].addEventListener('click', () => {  // в рот мне ноги, Девид Блейн!
+    if (!btns[1].classList.contains('red')){
+        btns[1].classList.add('red');
+    }else{
+        btns[1].classList.remove('red');
+    }
+    // btns[1].classList.toggle('red'); // тоже работает но 
+                                    //тоггл не всегда доступен
+});
+
+console.log(btns[0].className);
+
+// делегирование
+
+// wrapper.addEventListener('click', (event) => {
+//     // console.dir(event.target);
+//     if (event.target && event.target.tagName == "BUTTON"){
+//         console.log('Hello');
+//     }
+// });
+
+wrapper.addEventListener('click', (event) => {
+    // console.dir(event.target);
+    if (event.target && event.target.matches("button.red")){ // проверка на класс
+        console.log('Hello');
+    }
+});
+
+// то же самое что и где wrapper но не пашет динамическая кнопка
+// btns.forEach(btn => {
+//     btn.addEventListener('click', () => {
+//         console.log('Hello');
+//     });
+// });
+
+const btn = document.createElement('button');
+btn.classList.add('red');
+wrapper.append(btn); // здесь добавляем кнопку и она тоже делает Хеллоу
+
+
